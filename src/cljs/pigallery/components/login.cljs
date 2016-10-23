120603b0275376d50720705b7bf77e1971694c58
Evgeny Fomin <fominok@hotmail.com>
2016-10-23 17:35:09 +0300

    session timeout

--- src/cljs/pigallery/components/login.cljs
+++ src/cljs/pigallery/components/login.cljs
@@ -6,6 +6,16 @@
             [ajax.core :as ajax]
             [pigallery.components.common :as c]))
 
+(def timeout-ms (* 1000 60 30))
+
+(defn session-timer []
+  (when (session/get :identity)
+    (if (session/get :user-event)
+      (do
+        (session/remove! :user-event)
+        (js/setTimeout #(session-timer) timeout-ms))
+      (session/remove! :identity))))
+
 (defn encode-auth [user pass]
   (->> (str user ":" pass)
        (b64/encodeString)
@@ -20,6 +30,7 @@
          :handler #(do
                      (session/remove! :modal)
                      (session/put! :identity id)
+                     (js/setTimeout session-timer timeout-ms)
                      (reset! fields nil))
          :error-handler #(reset! error (get-in % [:response :message]))})))
 
